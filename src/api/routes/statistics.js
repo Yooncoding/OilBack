a37be3e7ebc5680810bb656214207fd216930149
윤철중 <cjfwnd123zz@gmail.com>
2022-10-30 20:51:14 +0900

    feat: 기간동안 감정의 평균 조회 기능 validator 추가

--- src/api/routes/statistics.js
+++ src/api/routes/statistics.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import auth from "../middlewares/auth/authorization";
 import StatisticsContoller from "../controllers/statistics";
+import { StatisticsValidator } from "../middlewares/validators/validation";
 
 const router = Router();
 
@@ -8,7 +9,7 @@ function statisticsRouter(root) {
   root.use("/statistics", router);
   router.use(auth.isLogin);
 
-  router.get("/", StatisticsContoller.getStatistics);
+  router.get("/", StatisticsValidator.getStatistics, StatisticsContoller.getStatistics);
 }
 
 export default statisticsRouter;
