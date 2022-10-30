7bb9d1970744c6dc99a24db4b1e1418e7e1a4c64
윤철중 <cjfwnd123zz@gmail.com>
2022-10-30 20:51:44 +0900

    modifiy: 기간동안 감정의 평균 조회 기능 response 변경

--- src/api/services/statistics.js
+++ src/api/services/statistics.js
@@ -10,21 +10,35 @@ const StatisticsService = {
     let negativeSum = 0;
     let neutralSum = 0;
     let cnt = 0;
+    let filteredPosts = [];
     posts.forEach((post) => {
       positiveSum += post.positive;
       negativeSum += post.negative;
       neutralSum += post.neutral;
       cnt += 1;
+
+      filteredPosts.push({
+        positive: post.positive,
+        negative: post.negative,
+        neutral: post.neutral,
+        yyyymmdd: post.yyyymmdd,
+      });
     });
 
-    const sentiment = Math.max(positiveSum, negativeSum, neutralSum);
+    const max = Math.max(positiveSum, negativeSum, neutralSum);
+    let maxSentiment = "";
+    if (max === positiveSum) maxSentiment = "positive";
+    if (max === negativeSum) maxSentiment = "negative";
+    if (max === neutralSum) maxSentiment = "neutral";
 
     const data = {
+      filteredPosts,
+      positiveAvg: positiveSum / cnt,
+      negativeAvg: negativeSum / cnt,
+      neutralAvg: neutralSum / cnt,
+      maxSentiment,
+      cnt,
       userId,
-      positive: positiveSum / cnt,
-      negative: negativeSum / cnt,
-      neutral: neutralSum / cnt,
-      sentiment,
     };
 
     return data;
